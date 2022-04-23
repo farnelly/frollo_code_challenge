7d07d789a5f511a5f22cb792bab1c5e7722f18e7
Pratik Ghimire <erpratikgh@gmail.com>
2022-04-23 14:02:33 +0545

    Add tests for transactions.

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -8,4 +8,17 @@ RSpec.describe Transaction, type: :model do
       expect(described_class.new).to be_valid
     end
   end
+
+  describe 'after_save' do
+    let!(:transaction) { build(:transaction) }
+  
+    it 'enqueues background job to categorise transaction' do
+      expect(CategorisationProcessJob).to receive(:perform_async)
+        .with(id: 1)
+
+      transaction.save
+
+      expect(transaction.category).to eq('Uncategorised')
+    end
+  end
 end
